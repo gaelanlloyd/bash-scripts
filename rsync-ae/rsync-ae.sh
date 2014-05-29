7156c78d383e0e5c6ac516c832f7c7980110aee6
gaelanlloyd <gaelanlloyd@gmail.com>
2014-05-29 13:25:56 -0700

    First push of rsync-ae skeleton file

---
+++ rsync-ae/rsync-ae.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# Purpose: Runs rsync on a given target and excludes any folder that
+#          contains a file called .exclude
+#
+# Copyright (C) 2014 Gaelan Lloyd
+#
+# rsync-ae is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ------------------------------------------------------------------
+
+# Definitions
+
+# Presence of this file in a folder will exclude the folder from the rsync operation
+excludeFlag=".exclude"
+
+
